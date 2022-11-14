d39e71f51d593c0131e9688c57f459890be49f9d
Romain-Pietri <romain.pietri@student.junia.com>
2022-11-14 15:58:24 +0100

    Sockets qui marchent
    C'est bon j'ai reussi

--- Banque_Centrale/fichier client/client.cpp
+++ Banque_Centrale/fichier client/client.cpp
@@ -1,38 +1,89 @@
 #include <iostream>
 #include <boost/asio.hpp>
-//g++ client.cpp -o client –lboost_system
+#include <thread>
+#include <fstream>
+#include <string>
+//g++ client.cpp -o client -L –lboost_system
 using namespace boost::asio;
 using ip::tcp;
 using std::string;
 using std::cout;
 using std::endl;
+
+string recup_info(){   
+      string id;
+      // ouvre le fichier
+
+      std::ifstream fichier("info_client.txt", std::ios::in);  // on ouvre en lecture
+      if(fichier)  // si l'ouverture a réussi
+      {
+            // instructions
+            getline(fichier, id);
+            fichier.close();
+      }
+      else  // sinon
+            std::cerr << "Impossible d'ouvrir le fichier !" << std::endl;
+      return id;
+}
+string read_bdd(){
+      return "0";
+}
+
+void send_to_serveur(string message){
+      boost::asio::io_service io_service;
+      //socket creation
+      tcp::socket socket(io_service);
+      //connection
+      socket.connect( tcp::endpoint( boost::asio::ip::address::from_string("127.0.0.1"), 1234 ));
+      // request/message from client
+      const string msg = message + "\n";
+      boost::system::error_code error;
+      boost::asio::write( socket, boost::asio::buffer(msg), error );
+      
+      
+}
+
+
+
+void client(){
+      string id= recup_info();
+      cout<<id<<endl;
+      
+     while(1){
+            boost::asio::io_service io_service;
+            //socket creation
+            tcp::socket socket(io_service);
+            //connection
+            socket.connect( tcp::endpoint( boost::asio::ip::address::from_string("127.0.0.1"), 1234 ));
+            // request/message from client
+            const string msg = "0/"+id+"\n";
+            boost::system::error_code error;
+            boost::asio::write( socket, boost::asio::buffer(msg), error );
+            // getting response from server
+            boost::asio::streambuf receive_buffer;
+            boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
+            if( error && error != boost::asio::error::eof ) {
+                  cout << "receive failed: " << error.message() << endl;
+            }
+            else {
+                  const char* data = boost::asio::buffer_cast<const char*>(receive_buffer.data());
+                  cout << data << endl;
+                  
+                  if(data[0]=='n'){
+                        //attend 0.1s
+                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+                        send_to_serveur("1/"+id+"/"+"bdd");
+                  }
+            }
+     sleep(3);
+     
+     }
+}
 int main() {
-     boost::asio::io_service io_service;
-//socket creation
-     tcp::socket socket(io_service);
-//connection
-     socket.connect( tcp::endpoint( boost::asio::ip::address::from_string("127.0.0.1"), 1234 ));
-// request/message from client
-     const string msg = "0/<LyceMons><LyceMon name=Mme Fer-et><ID>1</ID><Type>Francais</Type><TypeID>5</TypeID><PM>30</PM><EE>35</EE><CE>32</CE><EO>31</EO><CO>25</CO><C>20</C><Evo>2</Evo><EvoLvl>21</EvoLvl><Desc>Petites et frileuses les Mme Fer-et exercent cependant une autorit� naturelle sur les autres LycMons. Trs  lcoute elles respectent et suivent leur Etudiant mais nhsitent pas  le contester si elles pensent quil est en tord. </Desc><att_1>210/0</att_1><att_2>211/0</att_2><att_3>19/5</att_3><att_4>217/7</att_4><att_5>47/10</att_5><att_6>52/15</att_6><att_7>13/20</att_7><att_8>75/25</att_8><att_9>168/30</att_9><att_10>4/35</att_10><att_11>94/40</att_11><att_12>205/50</att_12><att_13>15/55</att_13><att_14>49/60</att_14><att_15>134/70</att_15><att_16>37/75</att_16><att_17>232/80</att_17><att_18>114/85</att_18><att_19>216/90</att_19><att_20>76/95</att_20><att_21>31/100</att_21><att_22>0/0</att_22></LyceMon>\n";
-     boost::system::error_code error;
-     boost::asio::write( socket, boost::asio::buffer(msg), error );
-     if( !error ) {
-        cout << "Client sent hello message!" << endl;
-     }
-    
-     else {
-        cout << "send failed: " << error.message() << endl;
-     }
-      cout<<socket.remote_endpoint().address()<<endl;
- // getting response from server
-    boost::asio::streambuf receive_buffer;
-    boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
-    if( error && error != boost::asio::error::eof ) {
-        cout << "receive failed: " << error.message() << endl;
-    }
-    else {
-        const char* data = boost::asio::buffer_cast<const char*>(receive_buffer.data());
-        cout << data << endl;
-    }
-    return 0;
+      //creer un thread pour le serveur
+      //std::thread t1(client);
+      //t1.join();
+      client();
+   return 0;
 }
--- Banque_Centrale/src/serveur.cpp
+++ Banque_Centrale/src/serveur.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 #include <boost/asio.hpp>
-
+#include <thread>
+#include <vector>
 using namespace boost::asio;
 using ip::tcp;
 using std::string;
 using std::cout;
 using std::endl;
+
+
+class Agence{
+      private : 
+      int id;
+      int nbappel;
+      public:
+      Agence(int id){
+            this->id=id;
+            this->nbappel=0;
+      }
+      int getid(){
+            return this->id;
+      }
+      int getnbappel(){
+            return this->nbappel;
+      }
+      void increment(){
+            this->nbappel++;
+            this->nbappel=this->nbappel%10;
+
+      }
+};
 
 
 string read_(tcp::socket & socket) {
@@ -19,27 +43,76 @@ void send_(tcp::socket & socket, const string& message) {
        boost::asio::write( socket, boost::asio::buffer(message) );
 }
 
+string lire(string message){
+      string res="";
+      for(int i=0;i<message.size();i++){
+            if(message[i]=='/'){
+                  break;
+            }
+            res+=message[i];
+      }
+      return res;
+}
+
+
 void push_BDD(string message){
     //TODO
 }
 
-void readmessage(string message){
+string readmessage(string message, std::vector<Agence> &agences){
       //type de message : Id demande/info demande;
+      string res;
+
       switch (message[0]){
-            case '0'://Mise a jour de BDD apres demande du serveur
+            case '0'://U ok ?
                   message.erase(0,2);
-                  push_BDD(message);
+                  res=message;
+                  
+                  //verifie si le vecteur est vide
+                  if(agences.empty()){
+                        Agence agence(stoi(res));
+                        agences.push_back(agence); 
+                        agences[0].increment();
+                        return "need";                       
+                  }
+                  else{
+                        //verifie si l'agence est deja dans le vecteur
+                        bool trouve=false;
+                        for(int i=0;i<agences.size();i++){
+                              if(agences[i].getid()==stoi(res)){
+                                    trouve=true;
+                                    agences[i].increment();
+                                    if(agences[i].getnbappel()==0){
+                                          return "need";
+                                    }
+                                    
+                                    break;
+                              }
+                        }
+                        if(!trouve){
+                              Agence agence(stoi(res));
+                              agences.push_back(agence);
+                              agences[agences.size()-1].increment();
+                              return "need";                              
+                        }
+                  }
+                  break;
+
+            case '1'://Mise a jour de BDD apres demande du serveur
+                  message.erase(0,2);
+                  //push_BDD(message);
+                  cout<<"BDD mise a jour"<<endl;
 
                   break;
-            case '1'://Ajout d'un client
+            case '2'://Ajout d'un client
                   message.erase(0,2);
                   //TODO
                   break;
-            case '2'://recherhce d'un client
+            case '3'://recherhce d'un client
                   message.erase(0,2);
                   //TODO
                   break;
-            case '3'://recherche du dernier ID Client
+            case '4'://recherche du dernier ID Client
                   message.erase(0,2);
                   //TODO
                   break;
@@ -53,26 +126,50 @@ void readmessage(string message){
                   break;
             
       }
+      return "0";
 }
 
 
 int main() {
+      cout<<"Serveur en ligne"<<endl;
+      std::vector<Agence> agences;
+      string renvoie;
+
     while(1){
       boost::asio::io_service io_service;
-//listen for new connection
+      //listen for new connection
       tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 1234 ));
-//socket creation 
+      //socket creation 
       tcp::socket socket_(io_service);
-//waiting for connection
+      //waiting for connection
       acceptor_.accept(socket_);
-//read operation
+      //read operation
       string message = read_(socket_);
       cout << message << endl;
-      readmessage(message);
-      cout<<socket_.remote_endpoint().address()<<endl;
+      renvoie=readmessage(message,agences);
+      
+      if(renvoie=="0"){
+            send_(socket_, "ok");
+      }
+      else{
+            send_(socket_, renvoie);
+      }
+
+
+      
+
+
+      
+
+      
+
+
+
+
 //write operation
-      send_(socket_, "Hello From Server!");
-      cout << "Servent sent Hello message to Client!" << endl;
+      //send_(socket_, "Hello From Server!");
+      //cout << "Servent sent Hello message to Client!" << endl;
+      //send_(socket_, "want to quit press q");
     }
    return 0;
 }
